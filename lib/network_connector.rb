da7132d7a0b1328eb6950de9539563e98a3f82cd
Mohamed Camara Gueye <katakeynii@gmail.com>
2022-04-14 08:17:19 +0000

    Initial commit

---
+++ lib/network_connector.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "network_connector/version"
+
+module NetworkConnector
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/network_connector_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe NetworkConnector do
+  it "has a version number" do
+    expect(NetworkConnector::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
